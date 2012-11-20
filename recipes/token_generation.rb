d70a1e6e2e513024e6f6446f66be4ffc99b63db2
Hayato Shimizu <hayato.shimizu@bskyb.com>
2012-11-20 17:55:08 +0000

    Removing global conditional from token generator

--- recipes/token_generation.rb
+++ recipes/token_generation.rb
@@ -13,39 +13,37 @@
 # 
 ###################################################
 
-if node[:cassandra][:initial_token] == false
 
-  # Write tokentool to a an executable file
-  cookbook_file "/tmp/tokentool.py" do
-    source "tokentool.py"
-    mode "0755"
-  end
+# Write tokentool to a an executable file
+cookbook_file "/tmp/tokentool.py" do
+  source "tokentool.py"
+  mode "0755"
+end
 
-  # Run tokentool accordingly
-  ruby_block "Run Tokentool" do
-    block do
-      node[:cassandra][:data_centres].each do |dc|
-        node[:cassandra][:cluster_size_list] = cluster_size_list + " " + dc[:cluster_size]
-      end
-      Chef::Log.info "Cassandra cluster sizes: #{node[:cassandra][:cluster_size_list]}"
+# Run tokentool accordingly
+ruby_block "Run Tokentool" do
+  block do
+    node[:cassandra][:data_centres].each do |dc|
+      node[:cassandra][:cluster_size_list] = cluster_size_list + " " + dc[:cluster_size]
     end
+    Chef::Log.info "Cassandra cluster sizes: #{node[:cassandra][:cluster_size_list]}"
   end
+end
 
-  execute "/tmp/tokentool.py #{ node[:cassandra][:cluster_size_list] } > /tmp/tokens" do
-    creates "/tmp/tokens"
-  end
-  
-  ruby_block "ReadTokens" do
-    block do
-      results = []
-      open("/tmp/tokens").each do |line|
-        results << line.chomp.split(':')[1].strip if line.include? 'Node'
-      end
+execute "/tmp/tokentool.py #{ node[:cassandra][:cluster_size_list] } > /tmp/tokens" do
+  creates "/tmp/tokens"
+end
 
-      Chef::Log.info "Setting token for this node."
-      private_ip = node[:cloud][:private_ips].first
-      token_position = node[:cassandra][:nodes][private_ip.to_str]
-      node[:cassandra][:initial_token] = results[node[:cassandra][token_position.to_str]]
+ruby_block "ReadTokens" do
+  block do
+    results = []
+    open("/tmp/tokens").each do |line|
+      results << line.chomp.split(':')[1].strip if line.include? 'Node'
     end
+
+    Chef::Log.info "Setting token for this node."
+    private_ip = node[:cloud][:private_ips].first
+    token_position = node[:cassandra][:nodes][private_ip.to_str]
+    node[:cassandra][:initial_token] = results[node[:cassandra][token_position.to_str]]
   end
 end
