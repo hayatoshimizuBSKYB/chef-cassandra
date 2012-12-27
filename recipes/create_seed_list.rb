47795a5621f2989118aea2f5dc3649c6bc183b55
Hayato Shimizu <hayato.shimizu@bskyb.com>
2012-12-27 18:57:53 +0000

    Changing seeding.

--- recipes/create_seed_list.rb
+++ recipes/create_seed_list.rb
@@ -13,43 +13,17 @@
 # 
 ###################################################
 
-# Calculate the seed list if not currently set
+
 if not node[:cassandra][:seed]
-
-  # Find the position of the current node in the ring
-  cluster_nodes = search(:node, "chef_environment:#{node[:cassandra][:environment]}")
-  if node[:cassandra][:token_position] == false
-    node[:cassandra][:token_position] = cluster_nodes.count
-  end
-  Chef::Log.info "Currently seen nodes: #{cluster_nodes.inspect}"
-
-  # Find all cluster node IP addresses
-  cluster_nodes_array = []
-  for i in (0..cluster_nodes.count-1)
-    cluster_nodes_array << [ cluster_nodes[i][:ohai_time], cluster_nodes[i][:cloud][:private_ips].first ]
-  end
-  cluster_nodes_array = cluster_nodes_array.sort_by{|node| node[0]}
-  Chef::Log.info "Currently seen nodes: #{cluster_nodes_array.inspect}"
-
   seeds = []
-
-  # Pull the seeds from the chef db
-  if cluster_nodes.count == 0
-    # Add this node as a seed since this is the first node
-    Chef::Log.info "[SEEDS] First node chooses itself."
-    seeds << node[:cloud][:private_ips].first
-  else
-    # Add the first node as a seed
-    Chef::Log.info "[SEEDS] Add all the nodes."
-    cluster_nodes_array.each do |seed_node|
-      seeds << seed_node[1]
-    end
+  node[:env_props][:nodes].each_pair do |key, value|
+    seeds << value[:ip_address]
   end
 else
-
   # Parse the seedlist into an array, if provided
   seeds = node[:cassandra][:seed].gsub(/ /,'').split(",")
 end
+
 
 Chef::Log.info "[SEEDS] Chosen seed(s): " << seeds.inspect
 
