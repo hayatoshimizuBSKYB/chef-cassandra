00eb4711d7d40e496fe12d8caf71624b002e8264
Hayato Shimizu <hayato.shimizu@bskyb.com>
2013-01-14 17:21:25 +0000

    Initial commit of snapshot backup recovery code

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,65 +20,65 @@ end
 
 bash "Add hosts entry" do
   code <<-EOH
-    if [ `grep #{ node[:cassandra][:ip_address] } /etc/hosts | wc -l` = 0 ]
+    if [ `grep #{node[:cassandra][:ip_address]} /etc/hosts | wc -l` = 0 ]
     then
       chattr -i /etc/hosts
-      echo #{ node[:cassandra][:ip_address] } `hostname` >> /etc/hosts
+      echo #{node[:cassandra][:ip_address]} `hostname` >> /etc/hosts
     fi
   EOH
 end
 
 # Create Cassandra User
-user "#{ node[:cassandra][:user] }" do
+user "#{node[:cassandra][:user]}" do
   comment "Cassandra User"
-  home "/home/#{ node[:cassandra][:user] }"
+  home "/home/#{node[:cassandra][:user]}"
   shell "/bin/bash"
 end
 
 # Download Cassandra from Apache
 
-remote_file "#{ node[:cassandra][:download_path]}/apache-cassandra-#{ node[:cassandra][:version] }-bin.tar.gz" do
-  source "#{ node[:cassandra][:download_url_base] }/#{ node[:cassandra][:version] }/apache-cassandra-#{ node[:cassandra][:version] }-bin.tar.gz"
+remote_file "#{node[:cassandra][:download_path]}/apache-cassandra-#{node[:cassandra][:version]}-bin.tar.gz" do
+  source "#{node[:cassandra][:download_url_base]}/#{node[:cassandra][:version]}/apache-cassandra-#{node[:cassandra][:version]}-bin.tar.gz"
   mode "0644"
   checksum node[:cassandra][:checksum]
 end
 
-directory "#{ node[:cassandra][:data_file_directories]}" do
-  owner "#{ node[:cassandra][:user] }"
-  group "#{ node[:cassandra][:group] }"
+directory "#{node[:cassandra][:data_file_directories]}" do
+  owner "#{node[:cassandra][:user]}"
+  group "#{node[:cassandra][:group]}"
   mode "0755"
   action :create
   recursive true
 end
 
-directory "#{ node[:cassandra][:commitlog_directory]}" do
-  owner "#{ node[:cassandra][:user] }"
-  group "#{ node[:cassandra][:group] }"
+directory "#{node[:cassandra][:commitlog_directory]}" do
+  owner "#{node[:cassandra][:user]}"
+  group "#{node[:cassandra][:group]}"
   mode "0755"
   action :create
   recursive true
 end
 
-directory "#{ node[:cassandra][:saved_caches_directory]}" do
-  owner "#{ node[:cassandra][:user] }"
-  group "#{ node[:cassandra][:group] }"
+directory "#{node[:cassandra][:saved_caches_directory]}" do
+  owner "#{node[:cassandra][:user]}"
+  group "#{node[:cassandra][:group]}"
   mode "0755"
   action :create
   recursive true
 end
 
-directory "#{ node[:cassandra][:log_path]}" do
-  owner "#{ node[:cassandra][:user] }"
-  group "#{ node[:cassandra][:group] }"
+directory "#{node[:cassandra][:log_path]}" do
+  owner "#{node[:cassandra][:user]}"
+  group "#{node[:cassandra][:group]}"
   mode "0755"
   action :create
   recursive true
 end
 
 
-directory "/home/#{ node[:cassandra][:user] }" do
-  owner "#{ node[:cassandra][:user] }"
-  group "#{ node[:cassandra][:group] }"
+directory "/home/#{node[:cassandra][:user]}" do
+  owner "#{node[:cassandra][:user]}"
+  group "#{node[:cassandra][:group]}"
   mode "0755"
   action :create
 end
@@ -86,20 +86,20 @@ end
 # Install Cassandra
 bash "Unpack Cassandra" do
   code <<-EOH
-    if [ ! -d #{ node[:cassandra][:install_path] }/apache-cassandra-#{ node[:cassandra][:version] } ]
+    if [ ! -d #{node[:cassandra][:install_path]}/apache-cassandra-#{node[:cassandra][:version]} ]
     then
-      cd #{ node[:cassandra][:install_path] }
-      tar xzf #{ node[:cassandra][:download_path]}/apache-cassandra-#{ node[:cassandra][:version] }-bin.tar.gz
-      ln -s #{ node[:cassandra][:install_path] }/apache-cassandra-#{ node[:cassandra][:version] } cassandra
+      cd #{node[:cassandra][:install_path]}
+      tar xzf #{node[:cassandra][:download_path]}/apache-cassandra-#{node[:cassandra][:version]}-bin.tar.gz
+      ln -s #{node[:cassandra][:install_path]}/apache-cassandra-#{node[:cassandra][:version]} cassandra
     fi
   EOH
 end
 
 # Download jna.jar
-remote_file "#{ node[:cassandra][:install_path] }/cassandra/lib/jna.jar }" do
-  source "http://repo1.maven.org/maven2/net/java/dev/jna/jna/#{ node[:cassandra][:jna_version] }/jna-#{ node[:cassandra][:jna_version] }.jar"
+remote_file "#{node[:cassandra][:install_path]}/cassandra/lib/jna.jar" do
+  source "http://repo1.maven.org/maven2/net/java/dev/jna/jna/#{node[:cassandra][:jna_version]}/jna-#{node[:cassandra][:jna_version]}.jar"
   mode "0644"
-  not_if { File.exists?("#{node[:cassandra][:install_path] }/cassandra/lib/jna.jar}") }
+  not_if { File.exists?("#{node[:cassandra][:install_path]}/cassandra/lib/jna.jar") }
 end
 
 # Install startup script from template
@@ -151,6 +151,52 @@ template "#{node[:cassandra][:install_path]}/cassandra/bin/snapshot.sh" do
    source "snapshot.sh.erb"
    notifies :restart , resources(:service => "cassandra")
 end
+
+ruby_block "Restore from snapshots" do
+  block do
+    require 'date'
+
+    # Is data volume empty?
+    if Dir.entries(node[:cassandra][:data_file_directories]).size < 2
+      # Check if snapshots exists
+      cassandra_backup_dir = "#{node[:cassandra][:backup_dir]}/cassandra"
+      if Dir.exists?(cassandra_backup_dir)
+        # Find the directory for latest backup date
+        latest_date_str = ""
+        latest_date_obj = Date.new("1970-01-01")
+        Dir.foreach(cassandra_backup_dir) do |date|
+          next if (keyspace.eql?(".")) || (keyspace.eql?(".."))
+          d = Date.new(date)
+          if d > latest_date_obj
+            latest_date_str = date
+            latest_date_obj = d
+          end
+        end
+        
+        # If backup directory is found
+        if latest_date_str.length > 0
+          Dir.foreach("#{cassandra_backup_dir}/#{latest_date_str}") do |keyspace|
+            next if (keyspace.eql?(".")) || (keyspace.eql?(".."))
+            
+            Dir.foreach("#{cassandra_backup_dir}/#{latest_date_str}/#{keyspace}") do |ks_component|
+              # Remove Cassandra keyspace component directory
+              FileUtils.rm_rf "#{node[:cassandra][:data_file_directories]}/#{keyspace}/#{ks_component}"
+              
+              # Copy the snapshot directory
+              FileUtils.cp_r "#{cassandra_backup_dir} /#{latest_date_str}/#{keyspace}/#{ks_component}", "#{node[:cassandra][:data_file_directories]}/#{keyspace}"
+              puts "Recovered #{keyspace}:#{ks_component} from backup."
+            end
+            
+          end
+        end
+        
+      end
+    end
+    
+  end
+  action :create
+end
+
 
 # Start Cassandra
 service "cassandra" do
